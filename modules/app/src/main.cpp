7cfa8a3d64bf5103991e4c455be68b74ce3a777c
tim95bell <tim95bell@gmail.com>
2026-02-22 20:34:34 +1100

    app: switch demo window title to new buffer API

--- modules/app/src/main.cpp
+++ modules/app/src/main.cpp
@@ -1,13 +1,16 @@
 #include <QApplication>
 #include <QString>
 #include <QWidget>
-#include <teks/teks.hpp>
+#include <teks/buffer/Buffer.hpp>
 
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
 
     QWidget window;
-    window.setWindowTitle(QString::fromStdString(teks::test()));
+    teks::buffer::Buffer buffer;
+    teks::buffer::insert_start(buffer,  "Hello from Teks");
+    const std::string title = teks::buffer::read_all_string(buffer);
+    window.setWindowTitle(QString::fromStdString(title));
     window.resize(800, 600);
     window.show();
 
--- modules/core/src/teks.cpp
+++
@@ -1,8 +0,0 @@
-#include <string>
-#include <teks/teks.hpp>
-
-namespace teks {
-    std::string test() {
-        return "Hello from Teks";
-    }
-}
